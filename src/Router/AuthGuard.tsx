01645297ed4f4e4e77f3a42a7ea641f117bffc3d
mateusbrbza <mateus.c.barboza@gmail.com>
2024-01-01 22:40:14 -0300

    feat: spinner no button, authguard e authcontext atualizados

--- src/Router/AuthGuard.tsx
+++ src/Router/AuthGuard.tsx
@@ -1,19 +1,20 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../app/hooks/useAuth";
 
 interface AuthGuardProps {
   isPrivate: boolean;
 }
 
 export function AuthGuard({ isPrivate }: AuthGuardProps) {
-  const signedIn = false
+  const { signedIn } = useAuth();
 
   if (!signedIn && isPrivate) {
-    return <Navigate to="/login" />
+    return <Navigate to="/login" />;
   }
 
   if (signedIn && !isPrivate) {
-    return <Navigate to="/" />
+    return <Navigate to="/" />;
   }
 
-  return <Outlet />
+  return <Outlet />;
 }
